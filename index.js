974092e043dbb54b8c59e6262fba2ecde00266c4
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-25 07:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function map(x,y){
 return r;
 }
 
-function reduce(x,y,c=0){
-  let r=c;
+function reduce(x,y){
+  let r=0;
   for (let i =0; i<x.length; i++){
     if (typeof(x[i])==='string' || typeof(x[i])==='boolean'){
    b=1;
